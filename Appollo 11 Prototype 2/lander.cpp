b175d1f5e4639a6ab2a5fecbb1be3752f9555578
Jacophobia <jacophilia@gmail.com>
2022-01-15 15:35:44 -0700

    Added comments, test cases, and finished live mode

--- Appollo 11 Prototype 2/lander.cpp
+++ Appollo 11 Prototype 2/lander.cpp
@@ -1,7 +1,24 @@
+/*****************************
+ * Source File:
+ *    lander.cpp
+ * Author:
+ *    Jacob Morgan and Jeremy Busch
+ * Summary:
+ *    This contains the definitions for the methods used by the Lander 
+ *    class. 
+ *****************************/
+
 #include "lander.h"
 #include <iostream>
 #include <stdexcept>
 
+ /*************************
+  * LANDER
+  * This constructor takes the initial vertical velocity, the initial 
+  * horizontal velocity, the initial altitude, and the angle of the 
+  * lander as parameters and sets the associated member variables 
+  * accordingly.
+  *************************/
 Lander::Lander(double vVelocity, double hVelocity, double altitude, double angle)
 {
 	this->vVelocity = vVelocity;
@@ -10,6 +27,12 @@ Lander::Lander(double vVelocity, double hVelocity, double altitude, double angle
 	this->angle = angle;
 }
 
+/*************************
+ * INCREMENT TIME
+ * This method increments the time forward by a specified number of 
+ * seconds and then calculates what the new velocities and positions 
+ * will be due to this change in time. 
+ *************************/
 void Lander::incrementTime(int seconds)
 {
 	this->seconds += seconds;
@@ -19,6 +42,11 @@ void Lander::incrementTime(int seconds)
 	updateAltitude(seconds);
 }
 
+/*************************
+ * DISPLAY STATUS
+ * This method displays the current status of the lander to the 
+ * console.
+ *************************/
 void Lander::displayStatus()
 {
 	std::cout.setf(std::ios::fixed | std::ios::showpoint);
@@ -33,17 +61,29 @@ void Lander::displayStatus()
 		<< angle << "deg" << std::endl;
 }
 
+/*************************
+ * CHANGE ANGLE
+ * This method updates the angle of the lander.
+ *************************/
 void Lander::changeAngle(double angle)
 {
 	this->angle = angle;
 }
 
+/*************************
+ * CONVERT DEGREES TO RADIANS
+ * This method takes in a value in degrees and returns it in radians.
+ *************************/
 double Lander::convertDegreesToRadians(double degrees)
 {
 	const double pi = 3.14159265358979323846;
 	return (2 * pi * degrees) / 360;
 }
 
+/*************************
+ * GET VERTICAL ACCELERATION
+ * This method returns the current vertical acceleration.
+ *************************/
 double Lander::getVerticalAcceleration()
 {
 	double radians = convertDegreesToRadians(angle);
@@ -51,6 +91,10 @@ double Lander::getVerticalAcceleration()
 	return computeAcceleration(force, weight, gravity);
 }
 
+/*************************
+ * GET HORIZONTAL ACCELERATION
+ * This method returns the current horizontal acceleration.
+ *************************/
 double Lander::getHorizontalAcceleration()
 {
 	double radians = convertDegreesToRadians(angle);
@@ -58,6 +102,11 @@ double Lander::getHorizontalAcceleration()
 	return computeAcceleration(force, weight) * 100;
 }
 
+/*************************
+ * COMPUTE ACCELERATION
+ * This method takes in the thrust, weight, and optionally gravity. It 
+ * then calculates and returns the resulting acceleration.
+ *************************/
 double Lander::computeAcceleration(double thrust, double weight, double gravity)
 {
 	//assert(thrust > 0.0);
@@ -68,22 +117,42 @@ double Lander::computeAcceleration(double thrust, double weight, double gravity)
 	return gravity + acceleration;
 }
 
+/*************************
+ * UPDATE VELOCITY
+ * This method updates the vertical and horizontal velocity based on 
+ * the amount that time has been incremented by.
+ *************************/
 void Lander::updateVelocity(int seconds)
 {
 	vVelocity = vVelocity + getVerticalAcceleration() * seconds;
 	hVelocity = hVelocity + getHorizontalAcceleration() * seconds;
 }
 
+/*************************
+ * COMPUTE TOTAL VELOCITY
+ * This method computes the total velocity using the pythagorean
+ * theorem on the vertical and horizontal velocity.
+ *************************/
 double Lander::computeTotalVelocity()
 {
 	return sqrt(vVelocity * vVelocity + hVelocity * hVelocity);
 }
 
+/*************************
+ * UPDATE ALTITUDE
+ * This method updates the altitude using the distance formula and the
+ * amount that time was incremented by.
+ *************************/
 void Lander::updateAltitude(int seconds)
 {
 	altitude = altitude + vVelocity * seconds + 0.5 * getVerticalAcceleration() * (seconds * seconds);
 }
 
+/*************************
+ * UPDATE X DISPLACEMENT
+ * This method updates the horizontal displacement using the distance
+ * formula and the amount that time was incremented by.
+ *************************/
 void Lander::updateHDisplacement(int seconds)
 {
 	xDisplacement = xDisplacement + hVelocity * seconds + 0.5 * getHorizontalAcceleration() * (seconds * seconds);
--- Appollo 11 Prototype 2/lander.h
+++ Appollo 11 Prototype 2/lander.h
@@ -1,22 +1,44 @@
+/*****************************
+ * Header File:
+ *    lander.h
+ * Author:
+ *    Jacob Morgan and Jeremy Busch
+ * Summary:
+ *    This file contains the declarations for the Lander class.
+ *****************************/
+
 #pragma once
+// Delete the following line to run in live mode.
+#define DEBUG
 #include <cassert>
 
-
+ /*************************
+  * Lander:
+  * This class represents the lander used during the Apollo mission. 
+  * It can be used to see what the position, speed, vertical velocity, 
+  * horizontal velocity, and angle of the lander are. You can increment 
+  * the time that has passed by any number of seconds and it will 
+  * update all of it's data to match the passing of time.
+  *************************/
 class Lander
 {
 public:
+	// Constructor
 	Lander(double vVelocity, double hVelocity, double altitude, double angle);
-	void incrementTime(int);
-	void displayStatus();
+	// Update Data
+	void incrementTime(int seconds);
 	void changeAngle(double angle);
-//#ifndef DEBUG
-//private:
-//#endif
+	// Show Progress
+	void displayStatus();
+#ifndef DEBUG
+private:
+#endif
+	// Constants
 	const double weight =  15103.00; // kg
 	const double gravity =   -1.625; // m/s^2
 	const double vThrust = 45000.00; // N
 	const double hThrust =   450.00; // N
-
+	// Variables
 	double vVelocity;
 	double hVelocity;
 	double angle;
@@ -24,13 +46,14 @@ public:
 	double xDisplacement = 0;
 	double yDisplacement = 0;
 	int seconds = 0;
-
+	// Calculations
+	double computeAcceleration(double thrust, double weight, double gravity = 0.0);
 	double convertDegreesToRadians(double degrees);
+	double computeTotalVelocity();
 	double getVerticalAcceleration();
 	double getHorizontalAcceleration();
-	double computeAcceleration(double thrust, double weight, double gravity = 0.0);
+	// Update Variables
 	void updateVelocity(int seconds);
-	double computeTotalVelocity();
 	void updateAltitude(int seconds);
 	void updateHDisplacement(int seconds);
 };
